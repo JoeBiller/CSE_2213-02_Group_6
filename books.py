8427dfbddae7c11a5ff8d0c1063f3128de6bf41b
csm477 <csm477@msstate.edu>
2023-04-27 18:13:37 -0500

    Updating my part

--- books.py
+++ books.py
@@ -1,2 +1,94 @@
-#Craig is working on this class.
-#Just making the file for later.
+#Books class
+
+class Books:
+    def __init__(self, db):
+        self.__db = db
+
+        self.__ISBN     = 0
+        self.__publisher = ""
+        self.__year    = 0
+        self.__genre  = ""
+        self.__title = ""
+        self.__price = 0.0
+        self.__amount = 0
+
+#Getters
+    def getGenre(self):
+        return self.__genre
+
+    def getPublisher(self):
+        return self.__publisher
+
+    def getYear(self):
+        return self.__year
+
+    def getTitle(self):
+        return self.__title
+
+    def getISBN(self):
+        return self.__ISBN
+
+    def getAmount(self):
+        return self.__amount
+
+    def __getBooksTuple(self):
+        return (self.__genre,
+            self.__publisher,
+            self.__year,
+            self.__title,
+            self.__ISBN,
+            self.__price,
+            self.__amount)
+
+#Setters
+    def setAmount(self, amount, isbn):
+        self.__db.cursor.execute("UPDATE books SET amount = {amount} WHERE ISBN = {isbn}")
+
+#Check if ISBN exists (Primary key of books, no duplicates)
+    def ISBNTaken(self, ISBN):
+        response = self.__db.cursor.execute("SELECT * FROM books WHERE ISBN = ?", (ISBN,))
+        if response.fetchone():
+            return True
+        else:
+            return False
+
+#Add book to inventory
+    def addBook(self, genre, publisher, year, title, ISBN, price, amount):
+        if not (self.ISBN(ISBN)):
+            self.__genre     = genre
+            self.__publisher = publisher
+            self.__year      = year
+            self.__title     = title
+            self.__ISBN      = isbn
+            self.__price     = price
+            self.__amount    = amount
+
+            self.__db.cursor.execute("INSERT INTO books VALUES(?, ?, ?, ?, ?, ?)", 
+            self.__getBooksTuple())
+            self.__db.connection.commit()
+
+            return True, f"Book {title} added. ISBN:{isbn}."
+        else:
+            return False, "A book with this ISBN already exists!"
+
+
+def addBookMenu(db, cr, br):
+
+    genre = input("Please input the genre of the book: ")
+    publisher = input("Please input the publisher of the book: ")
+    year = input("Please input the year of the book: ")
+    title = input("Please input the title of the book: ")
+    ISBN = input("Please input the ISBN of the book: ")
+    price = input("Please input the price of the book: ")
+    amount = input("Please input the amount of the book: ")
+
+
+    success, message = br.addBook(genre, publisher, year, title, ISBN, price, amount)
+
+    print()
+    print(message)
+
+    return None, None
+
+def addBookMessage(db, cr, br):
+    print("Adding new book")
--- main.py
+++ main.py
@@ -5,8 +5,9 @@ import order
 
 db = database.Database("project.db")
 cr = customer.Customer(db)
+br = books.Books(db)
 
-def leave(db, cr, code=0):
+def leave(db, cr, br, code=0):
     print("Exiting program.")
     exit(code)
 
@@ -17,9 +18,14 @@ menu_system = {
         "Exit":           leave
     },
     "Main Menu": {
+        "Edit Book Inventory": books.addBookMessage,
         "Edit Account": customer.infoMenu,
         "Logout":       customer.logoutMenu,
         "Exit":         leave
+    },
+    "Edit Book Inventory": {
+        "Add New Book": books.addBookMenu,
+        "Exit": leave
     },
     "Edit Account": {
         "Go Back":   "Main Menu",
@@ -34,7 +40,7 @@ menu_system = {
     }
 }
 
-def main(menu_system, db, cr):
+def main(menu_system, db, cr, br):
     current_menu_name = "Welcome Menu"
 
     custom_menu_name = None
@@ -88,9 +94,10 @@ def main(menu_system, db, cr):
             nonlocal custom_menu_options
             nonlocal db
             nonlocal cr
+            nonlocal br
 
             print()
-            menu, options = func_name(db, cr, *func_args)
+            menu, options = func_name(db, cr, br, *func_args)
 
             if options:
                 custom_menu_name = menu
@@ -118,7 +125,7 @@ def main(menu_system, db, cr):
 
 if __name__ == "__main__":
     try:
-        main(menu_system, db, cr)
+        main(menu_system, db, cr, br)
     except KeyboardInterrupt:
         print("Keyboard Interrupt")
         exit(1)
