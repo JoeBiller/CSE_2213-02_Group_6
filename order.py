6d163a7d8579f0b9b3906772c9786c5a901b3a98
aj1852 <112454674+aj1852@users.noreply.github.com>
2023-04-29 13:07:03 -0500

    Update order.py

--- order.py
+++ order.py
@@ -1,1 +1,72 @@
-# Anthony will do order class.
+
+from getpass import getpass
+
+class order:
+    def __init__(self, db):
+        self.__db = db
+     
+
+        self.__orderId =""
+        self.__username =""
+        self.__date =""
+        self.__shippingNumber=""
+        self.__isbn=""
+
+
+    def usernameTaken(self, username):
+         response = self.__db.cursor.execute("SELECT * FROM customers WHERE username = ?", (username,))
+         if response.fetchone():
+            return True
+         else:
+            return False
+    
+    def __getorderTuple(self):
+        return (self.__orderId,
+                self.__username,
+                self.__date,
+                self.__shippingNumber)
+    
+    def createOrder(self, orderId, username, date, shippingNumber, isbn):
+        if not self.__loggedIn:
+            if not (self.usernameTaken(username)):
+                self.__orderId = orderId
+                self.__username = username             
+                self.__date  = date
+                self.__shippingNumber = shippingNumber
+            
+                self.__db.cursor.execute("INSERT INTO orders VALUES(?, ?, ?, ?, ?, ?)", 
+                                         self.__getorderTuple())
+                self.__db.connection.commit()
+
+                self.__loggedIn = True
+
+                return True, f"Order created. Welcome {username}."
+            
+
+    
+
+            
+    def setUsername(self, username):
+        if self.__loggedIn:
+            if not username:
+                return False, "You must specify a username."
+            old_username = self.__username
+            if not self.usernameTaken(username):
+                self.__username = username
+                return True, f"Your username is now {self.__username}."
+            else:
+                return False, "Username is already taken."
+        else:
+            return False, "You are not logged in."           
+            
+
+    def getorderId(self):
+        return self.__orderId
+    
+    
+            
+
+    
+
+
+
