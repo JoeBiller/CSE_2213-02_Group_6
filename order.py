88f04aa88d630537da4d9a8e84ca59e5b7ddf314
aj1852 <112454674+aj1852@users.noreply.github.com>
2023-04-29 19:05:55 -0500

    Update order.py
    clean up order class

--- order.py
+++ order.py
@@ -1,60 +1,8 @@
 class order:
-    def __init__(self, db):
+    def __init__(self, db,crt):
         self.__db = db
-     
+        self.__crt = crt
 
-        self.__orderId =""
-        self.__username =""
-        self.__date =""
-        self.__shippingNumber=""
+    def createOrder(self, username):
+        pass
     
-    
-    def __getorderTuple(self):
-        return (self.__orderId,
-                self.__username,
-                self.__date,
-                self.__shippingNumber)
-    
-    def createOrder(self, orderId, username, date, shippingNumber):
-        if not self.__loggedIn:
-            if not (self.usernameTaken(username)):
-                self.__orderId = orderId
-                self.__username = username             
-                self.__date  = date
-                self.__shippingNumber = shippingNumber
-            
-                self.__db.cursor.execute("INSERT INTO orders VALUES(?, ?, ?, ?, ?, ?)", 
-                                         self.__getorderTuple())
-                self.__db.connection.commit()
-
-                self.__loggedIn = True
-
-                return True, f"Order created. Welcome {username}."
-            
-
-
-            
-    def setUsername(self, username):
-        if self.__loggedIn:
-            if not username:
-                return False, "You must specify a username."
-            old_username = self.__username
-            if not self.usernameTaken(username):
-                self.__username = username
-                return True, f"Your username is now {self.__username}."
-            else:
-                return False, "Username is already taken."
-        else:
-            return False, "You are not logged in."           
-            
-
-    def getorderId(self):
-        return self.__orderId
-    
-    
-            
-
-    
-
-
-
